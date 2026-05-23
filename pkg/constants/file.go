ab947dcd7e208098ae6118555b79193790f1c203
EG <2596166764@qq.com>
2026-05-23 22:13:29 +0800

    feat: add constant management

---
+++ pkg/constants/file.go
@@ -0,0 +1,8 @@
+package constants
+
+const (
+	AvatarExt           = ".jpg"
+	VideoExt            = ".mp4"
+	DateTimeFormat      = "2006-01-02 15:04:05"
+	FileNameDateFormat  = "20060102150405"
+)
---
+++ pkg/constants/redis.go
@@ -0,0 +1,7 @@
+package constants
+
+const (
+	RefreshTokenPrefix = "refresh:"
+	MFAKeyPrefix      = "mfa:"
+	PopularVideosKey   = "popular:videos"
+)
